ef4d02195d7d141ba48a590f729e027aa7215a96
StepanLozinskyi <stepanlozinskyi@gmail.com>
2022-10-13 18:29:59 +0300

    Adding first wdio test. Verify home page title

---
+++ test/specs/home.js
@@ -0,0 +1,14 @@
+// Practice E-Commerce Site – Automation Bro
+// https://practice.automationbro.com/
+
+describe("Home", () => {
+  it("Open URL & assert title", async () => {
+    // Open URL
+    await browser.url("https://practice.automationbro.com/");
+
+    // Assert title
+    await expect(browser).toHaveTitle(
+      "Practice E-Commerce Site – Automation Bro"
+    );
+  });
+});
